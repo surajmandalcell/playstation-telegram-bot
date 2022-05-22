9cbe8096fce8570972c2819dec9888dda725b19a
Suraj Mandal <surajmandalcell@gmail.com>
2022-05-22 18:31:34 +0530

    create first query

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import requests
 from dotenv import load_dotenv
 import logging
 from telegram import Update
@@ -8,6 +9,7 @@ from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
 load_dotenv()
 
 telegram_key = os.environ.get("TELEGRAM_KEY")
+platprices_key = os.environ.get("PLATPRICES_KEY")
 
 
 logging.basicConfig(
@@ -21,9 +23,21 @@ async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
     )
 
 
+async def game_search(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    game = update.message.text.replace("/game ", "")
+    data = requests.get(
+        f"https://platprices.com/api.php?key={platprices_key}&name={game}"
+    )
+    print(data.json())
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=f"Details for {game}"
+    )
+
+
 if __name__ == "__main__":
     application = ApplicationBuilder().token(telegram_key).build()
 
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("game", game_search))
 
     application.run_polling()
